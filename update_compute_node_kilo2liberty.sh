0fc09e847f6ba33db6b357d3ee9230d9b2e8f3bc
Marica Antonacci <marica.antonacci@ba.infn.it>
2017-02-21 21:11:12 +0100

    Update update_compute_node_kilo2liberty.sh

--- update_compute_node_kilo2liberty.sh
+++ update_compute_node_kilo2liberty.sh
@@ -3,10 +3,10 @@
 apt-get install -y software-properties-common
 add-apt-repository -y --remove cloud-archive:kilo
 add-apt-repository -y cloud-archive:liberty
-apt-get update
+apt-get update || exit 1
 apt-get install -y crudini
 
-apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -y
+apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -y || exit 2
 
 apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y nova-compute sysfsutils
 apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y neutron-plugin-linuxbridge-agent conntrack
@@ -17,3 +17,4 @@ crudini --set /etc/nova/nova.conf upgrade_levels compute 4.5
 
 service neutron-plugin-linuxbridge-agent restart
 service nova-compute restart
+
